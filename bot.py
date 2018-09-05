c11f33ab9fe4992f2cc648ce6078b0f30cd68726
michaelpugh1 <michaelowenpugh2013@gmail.com>
2018-09-05 12:20:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,10 +44,10 @@ async def on_ready():
     print('------')
     await bot.change_presence(game=discord.Game(name=game))
     
-    @client.event
-async def on_message(message):
-    if message.content.startswith(';ping'):
-        await client.send_message(message.channel, 'pong')
+ @client.event
+async def on_message(msg):
+    if msg.content.startsWith('ping'):
+      await client.send_message(msg.channel,   'Pong!')
 
     
 @bot.event
